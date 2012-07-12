db534cf916f7959351db52a92c4d8d2a5f9256e9
Alex Kovar <ajkovar@gmail.com>
2012-07-12 00:04:34 -0500

    fixed algorithm

--- init.rb
+++ init.rb
@@ -31,17 +31,6 @@ class Water
     @cave[@x+1][@y]==" "
   end
 
-  def space_below
-    y = @y
-    while(@cave[@x][y])
-      return true if @cave[@x][y]==" "
-      y=y+1
-    end
-
-    false
-  end
-      
-
   def move_down
     @y=@y+1
   end
@@ -61,32 +50,31 @@ class Water
   def is_in_open_spot
     @cave[@x][@y]!="~" and @cave[@x][@y]!="#"
   end
+
 end
 
-water_units = 4
+x=1
+y=1
 
 water_units.times do
-  water = Water.new(cave, 1, 1)
+  water = Water.new(cave, x, y)
 
-  while(!water.is_in_open_spot)
-    puts water.x
-    puts water.y
+  while(!water.is_in_open_spot or water.left_is_open)
     if(water.left_is_open)
-      puts "left open"
       water.move_left
-    elsif(water.space_below)
-      puts "bottom open"
+    elsif(water.bottom_is_open)
       water.move_down
     elsif(water.right_is_open)
-      puts "right open"
       water.move_right
     else
-      puts "move up"
       water.move_up
     end 
   end
+
+  x=water.x
+  y=water.y
   
-  cave[water.x][water.y] = "~"
+  cave[x][y] = "~"
 end
 
 cave[0].size.times do |index|
